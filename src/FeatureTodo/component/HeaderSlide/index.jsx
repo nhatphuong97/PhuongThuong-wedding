d1c240bf57056a2111812cb402f512746fcb0acd
Nhat Phuong <nhatphuong4697@gmail.com>
2023-11-18 12:23:54 +0700

    add change slide show

--- src/FeatureTodo/component/HeaderSlide/index.jsx
+++ src/FeatureTodo/component/HeaderSlide/index.jsx
@@ -4,25 +4,23 @@ import CountDownTime from "../CountDownTime";
 import { motion, AnimatePresence } from "framer-motion";
 
 const listImage = [
-  "https://i.imgur.com/JvhcUsv.jpg",
-  "https://i.imgur.com/E2Flqah.jpg",
-  "https://i.imgur.com/ymoNuJS.jpg",
-  "https://i.imgur.com/xVzvYGs.jpg",
+  "https://i.imgur.com/JMn4EKH.jpg",
+  "https://i.imgur.com/Q3xyZlF.jpg",
 ];
 const variable = {
   inital: {
-    opacity: 0.7,
+    opacity: 0,
     scale: 1,
   },
   animate: {
-    opacity: 1,
+    opacity: [0, 1, 1, 1, 0],
     scale: 1.2,
   },
-  exit: {
-    // x: -400,
-    scale: 1,
-    opacity: 0,
-  },
+  //   exit: {
+  //     // x: -400,
+  //     x: 2000,
+  //     opacity: 0,
+  //   },
 };
 
 function HeaderSlide(props) {
@@ -62,68 +60,41 @@ function HeaderSlide(props) {
   //     console.log(index);
   //   };
 
-  //   useEffect(() => {
-  //     setTimeout(function () {
-  //       next1();
-  //     }, 3000);
-  //   });
-
   useEffect(() => {
-    const interval = setInterval(() => {
-      console.log({ rereder: "hjahaah" });
-      next2();
-    }, 3000);
-
-    return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
-  }, []);
-
+    const id = setInterval(next2, 4000);
+    return () => clearInterval(id);
+  }, [index]);
   return (
     // {props.className}
     <div
-      className="flex w-full  lg:h-[900px]  md:h-[800px] h-[600px]  bg-local md:bg-fixed bg-cover overflow-hidden
+      className="flex w-full bg-black relative lg:h-[900px]  md:h-[800px] h-[600px]  bg-local md:bg-fixed bg-cover overflow-hidden
     bg-no-repeat bg-center] -z-10"
     >
       <div className="absolube w-full h-auto flex justify-between ">
-        {/* <div
-          className=" absolute left-0 text-white text-2xl font-medium bg-red-400  cursor-pointer"
-          onClick={prev1}
-        >
-          prev
-        </div> */}
-
         <motion.img
           className="w-full object-cover"
           variants={variable}
           initial="inital"
           animate="animate"
           loading="lazy"
-          transition={{ duration: 2 }}
+          transition={{ duration: 4, ease: "linear" }}
           src={listImage[index]}
           alt=""
           key={listImage[index]}
         />
-        <div
-          className=" absolute right-0 text-white text-2xl font-medium bg-red-400 cursor-pointer"
-          onClick={next1}
-        >
-          next
-        </div>
       </div>
-      {/* <div className="w-full relative h-full bg-black bg-opacity-20  flex flex-col justify-center ">
-        <div className="justify-center items-center flex ">
+      <div className="w-full absolute h-full bg-black bg-opacity-20  flex flex-col justify-center " />
+
+      <div className="w-full absolute h-full flex flex-col justify-center overflow-hidden ">
+        <div className="justify-center items-center flex flex-col">
           <div className="tw-name-main">Nhật Phương</div>
-          <img
-            loading="lazy"
-            src={logoHeader}
-            alt=""
-            className={` lg:h-[150px] h-[20%]`}
-          />
+          <img loading="lazy" src={logoHeader} alt="" className={` h-[10%]`} />
           <div className="tw-name-main">Hoài Thương</div>
         </div>
       </div>
       <div className=" flex absolute items-center bottom-0 left-0 right-0 mb-5 justify-center">
         <CountDownTime listData={12}></CountDownTime>
-      </div> */}
+      </div>
     </div>
   );
 }
